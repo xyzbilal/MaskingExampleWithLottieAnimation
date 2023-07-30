e0b2452d8acea93e1d41509d99cbd30e39dc160e
bilal simsek <xyzbilal@gmail.com>
2023-07-30 14:46:39 +0300

    Initial Commit

---
+++ Masking/MaskingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskingApp.swift
+//  Masking
+//
+//  Created by Bilal SIMSEK on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MaskingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
